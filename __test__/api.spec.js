3596e2e9c6642e4dde535164b2739531716307a4
Stefan Mircea <stefancmircea@gmail.com>
2022-08-26 14:59:38 +0100

    add tests for get

--- __test__/api.spec.js
+++ __test__/api.spec.js
@@ -21,4 +21,28 @@ describe('api server', () => {
             .expect(200, done)
     })
     
+    test('it responds to get /journal-entries with status 200', (done) => {
+        request(api)
+            .get('/journal-entries')
+            .expect(200, done)
+    })
+
+    test('responds to get entry id with status 200', (done) => {
+        request(api)
+            .get('/journal-entries/2')
+            .expect(200, done)
+    })
+
+    test('it responds to get /comments with status 200', (done) => {
+        request(api)
+            .get('/comments')
+            .expect(200, done)
+    })
+
+    test('responds to a unknown comment id with a 404', (done) => {
+        request(api)
+            .get('/comments/42')
+            .expect(404)
+            .expect({ message: 'That comment does not exist!' }, done)
+    })
 })
